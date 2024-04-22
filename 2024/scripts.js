0b47427e813b59ee6aca68f9035a2d03b2baa611
benlw <ben6105@163.com>
2024-04-22 19:46:11 +0800

    add 2024

--- 2024/scripts.js
+++ 2024/scripts.js
@@ -1,24 +1,35 @@
 window.MathJax = {
-    tex: {
-      inlineMath: [['$', '$']],
-      displayMath: [['$$', '$$'], ['\\[', '\\]']]
-    },
-    svg: {
-      fontCache: 'global'
-    },
-    loader: {
-      load: ['output/svg', 'input/tex']
-    }
-  };
+  tex: {
+    inlineMath: [['$', '$']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']]
+  },
+  svg: {
+    fontCache: 'global'
+  },
+  loader: {
+    load: ['output/svg', 'input/tex']
+  }
+};
 
-function toggleImage() {
-    var gif = document.getElementById('animation');
-    var png = document.getElementById('image');
-    if (gif.style.display === 'none') {
-        gif.style.display = 'block';
-        png.style.display = 'none';
-    } else {
-        gif.style.display = 'none';
-        png.style.display = 'block';
-    }
+function toggleImage(pngId, gifId, buttonId) {
+  var gif = document.getElementById(gifId);
+  var png = document.getElementById(pngId);
+  var button = document.getElementById(buttonId);
+
+  // 检查 GIF 是否已加载
+  if (gif.getAttribute('src') === '') {
+    // 如果没有，设置 GIF 的源
+    gif.setAttribute('src', gif.getAttribute('data-gif'));
+  }
+
+  // 切换图片的可见性
+  if (gif.style.display === 'none') {
+    gif.style.display = 'block';
+    png.style.display = 'none';
+    button.textContent = button.getAttribute('data-text'); // 设置按钮文本为“显示 PNG”
+  } else {
+    gif.style.display = 'none';
+    png.style.display = 'block';
+    button.textContent = '显示 GIF'; // 设置按钮文本为“显示 GIF”
+  }
 }
